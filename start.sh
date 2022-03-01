9f73f28378352db9f56c74c940f9402a745629f0
Peace <zutar1049@gmail.com>
2022-03-01 10:31:02 +0000

    Kill attack before creating a new one

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@
 attacks=("HTTP" "UDP" "NTP" "SYN" "POD" "ICMP" "Slowloris" "Memcached")
 
 for attack in ${attacks[@]}; do
-  tmux new-session -d -s "${attack}" "'./attack.sh --method ${attack} --time 50 --threads 20'"
+  tmux kill-session -t "${attack}" 2>/dev/null
+  tmux new-session -d -s "${attack}" "./attack.sh --method ${attack} --time 50 --threads 20"
 done
