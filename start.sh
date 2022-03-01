157f92b8823f524b6bd1d3d787fd86b65cfa3be9
Peace <zutar1049@gmail.com>
2022-03-01 16:18:25 +0000

    update start.sh to use split-window in tmux

--- start.sh
+++ start.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-attacks=("HTTP" "UDP" "NTP" "SYN" "POD" "ICMP" "Slowloris" "Memcached")
-
-for attack in ${attacks[@]}; do
-  tmux kill-session -t "${attack}" 2>/dev/null
-  tmux new-session -d -s "${attack}" "./attack.sh --method ${attack} --time 60 --threads 30"
-done
+tmux kill-session -t "peace" 2>/dev/null
+tmux new-session -d -s "peace" "./attack.sh --method UDP --time 50 --threads 20" \; \
+  split-window -dh "./attack.sh --method HTTP --time 50 --threads 20" \; \
+  split-window -dh "./attack.sh --method NTP --time 50 --threads 20" \; \
+  split-window -dh "./attack.sh --method SYN --time 50 --threads 20" \; \
+  split-window -dv -t 0 "./attack.sh --method POD --time 50 --threads 20" \; \
+  split-window -dv -t 2 "./attack.sh --method ICMP --time 50 --threads 20" \; \
+  split-window -dv -t 4 "./attack.sh --method Slowloris --time 50 --threads 20" \; \
+  split-window -dv -t 6 "./attack.sh --method Memcached --time 50 --threads 20" \; \
+attach
